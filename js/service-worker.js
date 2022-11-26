452c12bc057861b6a04018b532bd2cdf2925b1ae
chelovek-gorod <chelovek-gorod@yandex.by>
2022-11-26 19:40:54 +0300

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -7,11 +7,20 @@ const staticURLs = [
     './js/main.js'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCashName);
+    await cache.addAll(staticURLs)
+
     console.log('[sw]: install');
-    event.waitUntil(
-        caches.open(staticCashName).then( cache => cache.addAll(staticURLs) )
-    );
 });
 self.addEventListener('activate', event => console.log('[sw]: activate'));
-self.addEventListener('fetch', event => console.log('[sw]: fetch'));
+self.addEventListener('fetch', event => {
+    console.log('[sw]: fetch', event.request.url);
+
+    event.respondWith( cacheFirst(event.request) );
+});
+
+async function cacheFirst(request) {
+    const cached = await caches.match( request );
+    return cached ?? await fetch( request );
+}
