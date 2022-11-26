3fcc286983166c7ad731cf0c7cab1809b1ad1f2c
chelovek-gorod <chelovek-gorod@yandex.by>
2022-11-26 19:12:47 +0300

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -2,3 +2,4 @@
 
 self.addEventListener('install', event => console.log('[sw]: install'));
 self.addEventListener('activate', event => console.log('[sw]: activate'));
+self.addEventListener('fetch', event => console.log('[sw]: fetch'));
