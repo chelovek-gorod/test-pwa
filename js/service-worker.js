eb9f2608ef3c291d078c028890a0add8b3a6d7b8
chelovek-gorod <chelovek-gorod@yandex.by>
2022-11-26 19:30:14 +0300

    update

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,5 +1,17 @@
 'use strict';
 
-self.addEventListener('install', event => console.log('[sw]: install'));
+const staticCashName = 'static-cash';
+const staticURLs = [
+    'index.html',
+    './style/style.css',
+    './js/main.js'
+];
+
+self.addEventListener('install', event => {
+    console.log('[sw]: install');
+    event.waitUntil(
+        caches.open(staticCashName).then( cache => cache.addAll(staticURLs) )
+    );
+});
 self.addEventListener('activate', event => console.log('[sw]: activate'));
 self.addEventListener('fetch', event => console.log('[sw]: fetch'));
